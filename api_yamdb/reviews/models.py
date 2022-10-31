0e5c4259064664133d9af4051bb8ec098d8463b8
KateIndri <kate.indri@yandex.ru>
2022-11-01 01:15:04 +0300

    app reviews: added models, views, serializers, permissions, admin and proto url

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,64 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    title = models.IntegerField(default=0)
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.text[:50]
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        unique_together = ('title', 'author')
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        verbose_name='Ревью',
+        on_delete=models.CASCADE,
+        related_name='commens'
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:50]
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
---
+++ api_yamdb/reviews/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api_yamdb.reviews.views import CommentViewSet
+
+from .views import (
+    ReviewViewSet,
+    CommentViewSet
+)
+
+v1_router = DefaultRouter()
+v1_router.register(
+    r'title/(?P<title_id>\d+)/reviews/',
+    ReviewViewSet,
+    basename='review'
+)
+v1_router.register(
+    r'title/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
+    CommentViewSet,
+    basename='comment'
+)
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
