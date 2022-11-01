dde361719326c853a37af55166407e18d598779c
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2022-11-01 21:57:39 +0300

    Add v1/auth/signup/ endpoint

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = routers.DefaultRouter()
-
+:
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/auth/signup/', TokenObtainPairView.as_view(), name='signup'),
