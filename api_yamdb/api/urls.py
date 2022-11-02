2df26fb691ddb361d04c6dcf57b255bebabf9458
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2022-11-02 09:34:12 +0300

    v1/auth/token/ fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -23,5 +23,5 @@ v1_router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('v1/auth/signup/', TokenObtainPairView.as_view(), name='signup'),
+    path('v1/auth/token/', TokenObtainPairView.as_view(), name='token'),
 ]
