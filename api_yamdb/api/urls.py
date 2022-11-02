49f729217920648db5f00dbf9d8a1ca6d57789d6
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2022-11-02 08:45:21 +0300

    UserSerializer + UserViewSet, fix v1/auth/token/

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .views import UserViewSet
+
 router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/signup/', TokenObtainPairView.as_view(), name='signup'),
+    path('v1/auth/token/', TokenObtainPairView.as_view(), name='token'),
 ]
