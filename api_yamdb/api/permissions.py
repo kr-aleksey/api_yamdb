78d9d397b4e23d076f2b227ee4d3f9b6dd9e0f38
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2022-11-08 22:31:47 +0300

    fix is_admin, is_moderator

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -49,8 +49,8 @@ class UserAPIPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
         if view.action in ['list', 'create', 'destroy']:
-            return bool(user and user.is_authenticated and user.is_admin())
+            return bool(user and user.is_authenticated and user.is_admin)
         return bool(user and user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_admin()
+        return request.user.is_admin
